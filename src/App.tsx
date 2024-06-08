2e5251a1a1a3721b6857fdf985db0fb09c0eea84
Nuno Saraiva <nuno.saraiva@wacestudio.com>
2024-06-08 01:31:52 +0100

    added react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,63 @@
-import { Button, Card, Center, Group, Image, Stack, Text, TextInput, Title } from '@mantine/core';
-import { Alchemy, Network, TokenBalancesResponseErc20, TokenMetadataResponse, Utils } from 'alchemy-sdk';
+import { Button, Card, Center, Group, Image, Loader, Stack, Text, TextInput, Title } from '@mantine/core';
+import { useQuery } from '@tanstack/react-query';
+import { Alchemy, Network, Utils } from 'alchemy-sdk';
 import { FC, useState } from 'react';
 
 const App: FC = () => {
   const [userAddress, setUserAddress] = useState('0xDa52002ddB5ad541d1559466Fd7505c562480dD8');
-  const [results, setResults] = useState<TokenBalancesResponseErc20>();
-  const [hasQueried, setHasQueried] = useState(false);
-  const [tokenDataObjects, setTokenDataObjects] = useState<TokenMetadataResponse[]>([]);
 
-  async function getTokenBalance() {
-    const config = {
-      apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
-      network: Network.ETH_MAINNET,
-    };
+  const alchemy = new Alchemy({
+    apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
+    network: Network.ETH_MAINNET,
+  });
 
-    const alchemy = new Alchemy(config);
-    const data = await alchemy.core.getTokenBalances(userAddress);
+  const { data: tokenDataObjects, isLoading, isError, refetch } = useQuery({
+    queryKey: ["token_balance", userAddress],
+    queryFn: async () => {
+      const data = await alchemy.core.getTokenBalances(userAddress);
 
-    setResults(data);
+      const tokensPromises = [];
+      for (let i = 0; i < data.tokenBalances.length; i++) {
+        const tokenData = alchemy.core.getTokenMetadata(data.tokenBalances[i].contractAddress);
 
-    const tokenDataPromises = [];
+        tokensPromises.push(tokenData);
+      }
 
-    for (let i = 0; i < data.tokenBalances.length; i++) {
-      const tokenData = alchemy.core.getTokenMetadata(
-        data.tokenBalances[i].contractAddress
-      );
-      tokenDataPromises.push(tokenData);
-    }
+      const tokens = await Promise.all(tokensPromises);
 
-    const tokenDataObjects = await Promise.all(tokenDataPromises);
+      return tokens.map((o, i) => ({
+        ...o,
+        balance: data.tokenBalances[i].tokenBalance
+      }));
+    },
+    enabled: false
+  });
 
-    setTokenDataObjects(tokenDataObjects);
-    setHasQueried(true);
-  }
   return (
     <Center>
       <Stack>
         <Title>ERC-20 Indexer</Title>
-
         <TextInput
           label="Type an address"
           value={userAddress}
           onChange={e => setUserAddress(e.target.value)}
         />
-        <Button onClick={getTokenBalance}>
-          Show Tokens
-        </Button>
-
+        <Button onClick={() => refetch()}>Get tokens</Button>
         {
-          hasQueried && results &&
-          results.tokenBalances.map((token, i) => {
-            const tokenBalance = Number(token.tokenBalance);
-            const decimals = Number(tokenDataObjects[i].decimals);
-            console.log("tokenBalance", tokenBalance);
-            console.log("decimals", decimals);
-            console.log(Utils.formatUnits(tokenBalance, decimals));
-
+          isLoading && <Center><Loader /></Center>
+        }
+        {
+          isError && <Text>No tokens found</Text>
+        }
+        {
+          tokenDataObjects &&
+          tokenDataObjects.map(token => {
             return (
-              <Card key={i}>
+              <Card key={token.name}>
                 <Stack>
-                  <Image src={tokenDataObjects[i].logo} />
-                  <Text>Token: {tokenDataObjects[i].symbol}</Text>
-                  {/* <Text>Balance: {Utils.formatUnits(Number(token.tokenBalance), Number(tokenDataObjects[i].decimals))}</Text> */}
+                  <Image w={50} src={token.logo} />
+                  <Text>Token: {token.symbol}</Text>
+                  <Text>Balance: {Utils.formatUnits(Number(token.balance).toString(), Number(token.decimals))}</Text>
                   <Group></Group>
                 </Stack>
               </Card>
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
 import { createTheme, MantineProvider } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '@mantine/core/styles.css';
+
+const queryClient = new QueryClient();
 
 const theme = createTheme({
   /** Put your mantine theme override here */
@@ -11,7 +14,9 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <MantineProvider theme={theme} defaultColorScheme='dark'>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </MantineProvider>
   </React.StrictMode>,
 )
